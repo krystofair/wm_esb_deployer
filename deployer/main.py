068d3150bdccbd192497ba95d1a0ff6ed5dee2ac
Krzysztof Kłapyta <krzysztof.klapyta@comarch.com>
2024-05-23 11:09:37 +0200

    version 2.1.2
    - fix get zone var using config function.

--- deployer/main.py
+++ deployer/main.py
@@ -101,7 +101,7 @@ def action_deploy(inbound=False) -> bool:
             name = nf.rstrip('.cfg')
             log.info("Load configuration for node {}".format(name))
             config.load_node_configuration(env, name)
-            nzone = config.get_env_var_or_default(os.environ[settings.ZONE_ENV_VAR], default='kokianowy_rbaon_astoarnuta')
+            nzone = config.get_env_var_or_default(settings.ZONE_ENV_VAR, default='kokianowy_rbaon_astoarnuta')
             nhost = os.environ[settings.SSH_ADDRESS_ENV_VAR]
             used_addresses = [addr for addr, zone in nodes_info.values()]
             if nhost in used_addresses:
